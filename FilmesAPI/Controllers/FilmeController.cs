d1cbc17b6094f490c293fcd92a71b6fd3397fe3b
Pedro Ivo Botter Ribas <pedro.ribas@bompracredito.com.br>
2022-12-19 16:40:42 -0400

    Cria modelo filme e controlador

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,38 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")] // CMT: a rota é o nome precedente ao sufixo Controller
+public class FilmeController : ControllerBase // CMT: extende ControllerBase
+{
+    // CMT: inicializar lista para guardar dados in-memory
+    private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
+
+    // # Adicionar recurso na DB
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        filme.Id = id++;
+        filmes.Add(filme);
+    }
+
+    // # Recuperar/ler recursos da DB
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilmes()
+    {
+        return filmes;
+
+        // CMT: a opção por retornar um IEnumerable em vez de uma List
+        // é melhor, pois IEnumerable é mais abstrato, e assim previne
+        // ter de refatorar o código caso a tipagem de Filmes mude
+    }
+
+    [HttpGet("{id}")] // CMT: recebe o valor id como parâmetro da requisição
+    public Filme? RecuperaFilmePorId(int id)
+    {
+        return filmes.FirstOrDefault(filme => filme.Id == id);
+    }
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+// CMT: modelo é a representação do mundo real
+
+// CMT: Data Annotations são atributos de validação usados
+// no model para criar restrições à requisição enviada pelo usuário.
+// Caso ocorra erro na validação, uma resposta JSON
+// é retornada com status 400 (Bad Request) e a mensagem de erro.
+
+public class Filme
+{
+    // # Id
+    public int Id { get; set; }
+
+    // # Título
+    // Valor não pode ser nulo ou vazio
+    [Required(ErrorMessage = "Título do filme é obrigatório")]
+    public string Titulo { get; set; }
+    
+    // # Gênero
+    // Valor não pode ser nulo ou vazio
+    [Required()]
+    // Valor deve possuir até 50 caracteres
+    [MaxLength(50, ErrorMessage = "Gênero deve conter até 50 caracteres")]
+    public string Genero { get; set; }
+    
+    // # Duração
+    // Valor não pode ser nulo ou vazio
+    [Required(ErrorMessage = "Duração do filme é obrigatório")]
+    // Valor deve estar entre 70 e 300
+    [Range(70, 300, ErrorMessage = "Duração deve ser de 70 a 300 minutos")]
+    public int Duracao { get; set; }
+}
